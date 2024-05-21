fe0870ab1eb93e2692bd0293fd0b081f5cb7877b
Vlad Beltran <puentes.studio@gmail.com>
2024-05-21 22:08:07 +0200

    projects grid working, no styles

--- src/app/works/works.component.ts
+++ src/app/works/works.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-works',
   templateUrl: './works.component.html',
-  styleUrl: './works.component.css'
+  // styleUrls:   // Corrected property name
 })
-export class WorksComponent {
+export class WorksComponent implements OnInit {
+  projects = {
+    data: [
+      {
+        list: [
+          {
+            name: 'Your Cloud',
+            imageUrl: '../../assets/projects/logotype.png', // Ensure you provide the correct path
+            projectLink: '',
+            repositoryLink: '',
+          },
+          {
+            name: 'Unika HR',
+            imageUrl: '../../assets/projects/unika.png', // Ensure you provide the correct path
+            projectLink: 'https://unikahr.com/',
+            repositoryLink: '',
+          },
+        ],
+      },
+      {
+        list: [
+          {
+            name: 'Pokemon Desk',
+            imageUrl: '../../assets/projects/pokemon.webp',
+            projectLink: 'https://pokemon-black-sable.vercel.app/',
+            repositoryLink: 'https://github.com/puentes-studio/PokemonBlack',
+          },
+        ],
+      },
+    ],
+  };
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
