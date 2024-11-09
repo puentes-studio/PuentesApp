6ef8aa826f848bd17851007f2204785e0e20c780
Vlad Beltran <puentes.studio@gmail.com>
2024-11-09 19:11:51 +0100

    cleaning script img

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.css'
+  styleUrl: './about-me.component.css',
 })
-export class AboutMeComponent {
-
-}
+export class AboutMeComponent {}
