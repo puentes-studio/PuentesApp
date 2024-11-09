bec4a9ad2f31945252dac0b5db94fde1071884bb
Vlad Beltran <puentes.studio@gmail.com>
2024-11-09 19:50:40 +0100

    animation about me img

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -5,4 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css',
 })
-export class AboutMeComponent {}
+export class AboutMeComponent {
+  // Method to change the image when mouse is over the img
+  changeImage(newSrc: string) {
+    const imgElement = document.querySelector(
+      'img.about-img'
+    ) as HTMLImageElement;
+    if (imgElement) {
+      imgElement.src = newSrc; // Change to the hover image
+    }
+  }
+
+  // Method to reset the image when mouse leaves
+  resetImage() {
+    const imgElement = document.querySelector(
+      'img.about-img'
+    ) as HTMLImageElement;
+    if (imgElement) {
+      imgElement.src = 'assets/img/dev-img01.png'; // Reset to the original image
+    }
+  }
+}
