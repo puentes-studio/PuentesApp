0bcd47001c5c55d2659a67cb9e8d558f09ce4e4f
Vlad Beltran <puentes.studio@gmail.com>
2025-01-16 14:02:00 +0100

    fixed style details at home

--- src/app/about-page/about-page.component.ts
+++ src/app/about-page/about-page.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about-page',
   templateUrl: './about-page.component.html',
-  styleUrl: './about-page.component.css'
+  styleUrl: './about-page.component.css',
 })
 export class AboutPageComponent {
+  // Method to change the image when mouse is over the img
+  changeImage(newSrc: string) {
+    const imgElement = document.querySelector(
+      'img.about-img'
+    ) as HTMLImageElement;
+    if (imgElement) {
+      imgElement.src = newSrc; // Change to the hover image
+    }
+  }
 
+  // Method to reset the image when mouse leaves
+  resetImage() {
+    const imgElement = document.querySelector(
+      'img.about-img'
+    ) as HTMLImageElement;
+    if (imgElement) {
+      imgElement.src = 'assets/img/dev-img01.png'; // Reset to the original image
+    }
+  }
 }
