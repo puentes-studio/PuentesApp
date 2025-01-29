3b6ed7ce7dad1286fc85a4e63c5c0181dadc653e
Vlad Beltran <puentes.studio@gmail.com>
2025-01-29 15:47:14 +0100

    carousel build but logic and library are not working

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import EmblaCarousel, { EmblaOptionsType } from 'embla-carousel';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrl: './carousel.component.css'
+  styleUrls: ['./carousel.component.css'],
 })
-export class CarouselComponent {
+export class CarouselComponent implements AfterViewInit {
+  projects = {
+    data: [
+      {
+        list: [
+          {
+            name: 'Taller Cinco Degree',
+            year: '2005',
+            imageUrl: '../../assets/png-cert/Cert-01.png',
+          },
+          {
+            name: 'SENA Diploma',
+            year: '2008',
+            imageUrl: '../../assets/png-cert/Cert-02.png',
+          },
+          {
+            name: 'Adobe Creative',
+            year: '2011',
+            imageUrl: '../../assets/png-cert/Cert-03.png',
+          },
+          {
+            name: 'Yamaha Music School',
+            year: '2019',
+            imageUrl: '../../assets/png-cert/Cert-04.png',
+          },
+        ],
+      },
+    ],
+  };
 
+  @ViewChild('emblaViewport', { static: false }) emblaViewport!: ElementRef;
+  embla: any; // Reference to the Embla carousel instance
+
+  options: EmblaOptionsType = {
+    loop: true,
+    speed: 10, // Use numbers between 0-100 for smoother behavior
+  };
+
+  ngAfterViewInit() {
+    if (this.emblaViewport?.nativeElement) {
+      this.embla = EmblaCarousel(
+        this.emblaViewport.nativeElement,
+        this.options
+      );
+    }
+  }
 }
