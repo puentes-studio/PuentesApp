a657c448759d7add116d92ebbfd46fbfb952a3cd
Vlad Beltran <puentes.studio@gmail.com>
2025-02-27 14:00:18 +0000

    projects descriptions done

--- src/app/work-view/work-view.component.ts
+++ src/app/work-view/work-view.component.ts
@@ -34,7 +34,7 @@ export class WorkViewComponent implements OnInit {
             projectAbout:
               'Unika HR is a performance HR entreperneur based in London. The company has been active in the britanic market since 2016 and is an official Premier Partner of Camm & Hooper.',
             projectChallengue:
-              'As a Designer and Developer, I was assigned the task of updating their visual identity and build their entire website',
+              'As a Designer and Developer, I was assigned the task of create their visual identity and build their entire website',
             projectSolution:
               'In alignment with Unika HR brand, which emphasizes people, transformation, and professional growth, we crafted brand elements and seamlessly integrated a refreshed look and feel into the corporate website.',
             backgroundColor: 'bg-purple-800',
@@ -60,11 +60,12 @@ export class WorkViewComponent implements OnInit {
             projectLink: '',
             repositoryLink: '',
             projectAbout:
-              'Unika HR is a performance HR entreperneur based in London. The company has been active in the britanic market since 2016 and is an official Premier Partner of Camm & Hooper.',
+              'Puentes Studio is a creative agency specializing in graphic design and video editing, the studio brings bold ideas to life through stunning visuals and compelling motion content.',
             projectChallengue:
-              'As a Designer and Developer, I was assigned the task of updating their visual identity and build their entire website',
+              'As the sole Designer and Developer, I was responsible for building the brand from the ground up—creating the logo, defining the visual identity, and developing the entire website.',
             projectSolution:
-              'In alignment with Unika HR brand, which emphasizes people, transformation, and professional growth, we crafted brand elements and seamlessly integrated a refreshed look and feel into the corporate website.',
+              'From branding to web development, I designed a cohesive and dynamic look that reflects Puentes Studio’s creative spirit. The website seamlessly integrates the brand’s aesthetic, providing a visually engaging and intuitive user experience.',
+
             backgroundColor: 'bg-orange-300',
             fontColor: 'text-lime-400',
           },
@@ -88,11 +89,12 @@ export class WorkViewComponent implements OnInit {
             projectLink: '',
             repositoryLink: '',
             projectAbout:
-              'Unika HR is a performance HR entreperneur based in London. The company has been active in the britanic market since 2016 and is an official Premier Partner of Camm & Hooper.',
+              'Your Cloud is a prototype for a cloud-based hard drive solution, designed to provide seamless file storage and access with an user-friendly interface.',
             projectChallengue:
-              'As a Designer and Developer, I was assigned the task of updating their visual identity and build their entire website',
+              'As a UX/UI Designer and Developer, I was responsible for designing the entire interface and user experience, while also collaborating heavily on both the front-end and back-end development, including API integration.',
             projectSolution:
-              'In alignment with Unika HR brand, which emphasizes people, transformation, and professional growth, we crafted brand elements and seamlessly integrated a refreshed look and feel into the corporate website.',
+              'By focusing on intuitive design and smooth functionality, I crafted a modern and efficient interface that enhances user interaction. The project also involved API fetching and backend collaboration, ensuring a seamless and responsive cloud storage experience.',
+
             backgroundColor: 'bg-yellow-500',
             fontColor: 'text-lime-200',
           },
@@ -113,11 +115,11 @@ export class WorkViewComponent implements OnInit {
             projectLink: '',
             repositoryLink: '',
             projectAbout:
-              'Unika HR is a performance HR entreperneur based in London. The company has been active in the britanic market since 2016 and is an official Premier Partner of Camm & Hooper.',
+              'Camm & Hooper Intranet is a private social portal designed for company employees, providing a space for communication, collaboration, and internal engagement.',
             projectChallengue:
-              'As a Designer and Developer, I was assigned the task of updating their visual identity and build their entire website',
+              'As the UX/UI Designer and Developer, I was responsible for designing the entire interface and user experience, as well as developing the full front-end, including a secure password-protected login system.',
             projectSolution:
-              'In alignment with Unika HR brand, which emphasizes people, transformation, and professional growth, we crafted brand elements and seamlessly integrated a refreshed look and feel into the corporate website.',
+              'By creating an intuitive and engaging design, I built a social platform that enhances employee interaction and connectivity. The intranet seamlessly integrates Camm & Hooper’s brand identity while ensuring smooth navigation and accessibility for users.',
             backgroundColor: 'bg-yellow-500',
             fontColor: 'text-lime-200',
           },
@@ -136,11 +138,12 @@ export class WorkViewComponent implements OnInit {
             projectLink: 'https://pokemon-black-sable.vercel.app/',
             repositoryLink: 'https://github.com/puentes-studio/PokemonBlack',
             projectAbout:
-              'Unika HR is a performance HR entreperneur based in London. The company has been active in the britanic market since 2016 and is an official Premier Partner of Camm & Hooper.',
+              'Pokémon Desk is a fun and interactive web app designed for Pokémon enthusiasts, allowing users to search and explore their favorite Pokémon with a sleek and engaging interface.',
             projectChallengue:
-              'As a Designer and Developer, I was assigned the task of updating their visual identity and build their entire website',
+              'As the sole UX/UI Designer and Developer, I designed and built the entire interface from scratch, ensuring a playful and intuitive experience while integrating API fetching for real-time Pokémon search.',
             projectSolution:
-              'In alignment with Unika HR brand, which emphasizes people, transformation, and professional growth, we crafted brand elements and seamlessly integrated a refreshed look and feel into the corporate website.',
+              'With a vibrant and dynamic design, I created an immersive experience that captures the essence of the Pokémon world. The app seamlessly fetches data from the Pokémon API, providing smooth and responsive interactions for users.',
+
             backgroundColor: 'bg-orange-700',
             fontColor: 'text-orange-200',
           },
@@ -160,11 +163,11 @@ export class WorkViewComponent implements OnInit {
             projectLink: '',
             repositoryLink: '',
             projectAbout:
-              'Unika HR is a performance HR entreperneur based in London. The company has been active in the britanic market since 2016 and is an official Premier Partner of Camm & Hooper.',
+              'Molly The Kid is a music artist with a bold and unique identity, blending visuals and sound to create a strong presence across digital platforms.',
             projectChallengue:
-              'As a Designer and Developer, I was assigned the task of updating their visual identity and build their entire website',
+              'I was tasked with redesigning the artist’s logo, creating a visually striking Spotify banner with photo editing, and designing the cover art for a single release on streaming platforms.',
             projectSolution:
-              'In alignment with Unika HR brand, which emphasizes people, transformation, and professional growth, we crafted brand elements and seamlessly integrated a refreshed look and feel into the corporate website.',
+              'By refining the brand identity with a fresh, modern logo and crafting high-quality visuals, I enhanced Molly The Kid’s digital presence. The redesigned elements seamlessly align with the artist’s style, creating a cohesive and impactful aesthetic for music streaming platforms.',
             backgroundColor: 'bg-fuchsia-900',
             fontColor: 'text-fuchsia-200',
           },
@@ -183,11 +186,12 @@ export class WorkViewComponent implements OnInit {
             projectLink: '',
             repositoryLink: '',
             projectAbout:
-              'Unika HR is a performance HR entreperneur based in London. The company has been active in the britanic market since 2016 and is an official Premier Partner of Camm & Hooper.',
+              'Cube Editorial is a fashion publication showcasing cutting-edge trends and artistic photography, highlighting the vibrant fashion scene in Bogotá, Colombia.',
             projectChallengue:
-              'As a Designer and Developer, I was assigned the task of updating their visual identity and build their entire website',
+              'As an editor for a special fashion issue, I was responsible for photo editing and post-production, refining visuals to align with the magazine’s high-end aesthetic.',
             projectSolution:
-              'In alignment with Unika HR brand, which emphasizes people, transformation, and professional growth, we crafted brand elements and seamlessly integrated a refreshed look and feel into the corporate website.',
+              'Through meticulous editing and post-production work, I enhanced the imagery to ensure a polished and visually striking final product. The result was a cohesive and dynamic fashion issue that captured the essence of contemporary style in Bogotá.',
+
             backgroundColor: 'bg-fuchsia-900',
             fontColor: 'text-fuchsia-200',
           },
@@ -205,11 +209,12 @@ export class WorkViewComponent implements OnInit {
             projectLink: '',
             repositoryLink: '',
             projectAbout:
-              'Unika HR is a performance HR entreperneur based in London. The company has been active in the britanic market since 2016 and is an official Premier Partner of Camm & Hooper.',
+              'De Buen Corazón was a campaign by Sanofi focused on raising awareness and promoting heart health through impactful visual storytelling.',
             projectChallengue:
-              'As a Designer and Developer, I was assigned the task of updating their visual identity and build their entire website',
+              'As the lead designer, I created the entire visual identity from scratch, including the logo and all campaign materials, ensuring a cohesive and engaging design language.',
             projectSolution:
-              'In alignment with Unika HR brand, which emphasizes people, transformation, and professional growth, we crafted brand elements and seamlessly integrated a refreshed look and feel into the corporate website.',
+              'By crafting a strong, heart-centered brand identity, I developed a visually compelling campaign that effectively communicated Sanofi’s message. The design elements were tailored to resonate with the audience, reinforcing the importance of cardiovascular health.',
+
             backgroundColor: 'bg-red-500',
             fontColor: 'text-red-100',
           },
@@ -226,11 +231,12 @@ export class WorkViewComponent implements OnInit {
             projectLink: '',
             repositoryLink: '',
             projectAbout:
-              'Unika HR is a performance HR entreperneur based in London. The company has been active in the britanic market since 2016 and is an official Premier Partner of Camm & Hooper.',
+              'OA Forum was a campaign by Sanofi Aventis, designed to foster discussion and awareness around osteoarthritis, providing valuable insights for healthcare professionals and patients.',
             projectChallengue:
-              'As a Designer and Developer, I was assigned the task of updating their visual identity and build their entire website',
+              'As the lead designer, I was responsible for creating the entire visual identity from scratch, including the logo and all supporting materials, ensuring a professional and engaging aesthetic.',
             projectSolution:
-              'In alignment with Unika HR brand, which emphasizes people, transformation, and professional growth, we crafted brand elements and seamlessly integrated a refreshed look and feel into the corporate website.',
+              'By developing a clean and informative design, I crafted a cohesive brand identity that effectively communicated the campaign’s message. The visuals were designed to be both accessible and impactful, aligning with Sanofi Aventis’ mission of education and healthcare awareness.',
+
             backgroundColor: 'bg-red-500',
             fontColor: 'text-red-100',
           },
@@ -243,11 +249,12 @@ export class WorkViewComponent implements OnInit {
             projectLink: '',
             repositoryLink: '',
             projectAbout:
-              'Unika HR is a performance HR entreperneur based in London. The company has been active in the britanic market since 2016 and is an official Premier Partner of Camm & Hooper.',
-            projectChallengue:
-              'As a Designer and Developer, I was assigned the task of updating their visual identity and build their entire website',
+              'Juntos en Biocirugía was a Sanofi campaign aimed at promoting innovation and collaboration in the field of biotherapy and surgical care.',
+            projectChallenge:
+              'I was responsible for designing the campaign’s logos, ensuring they visually represented the core values of precision, innovation, and medical excellence.',
             projectSolution:
-              'In alignment with Unika HR brand, which emphasizes people, transformation, and professional growth, we crafted brand elements and seamlessly integrated a refreshed look and feel into the corporate website.',
+              'Through thoughtful design, I created logos that seamlessly aligned with Sanofi’s branding while reinforcing the campaign’s message. The final visuals captured the essence of collaboration and advancement in biotherapy and surgery.',
+
             backgroundColor: 'bg-red-500',
             fontColor: 'text-red-100',
           },
