c97175b71aa7e100cf6e551bd996dd291d9289b6
Vlad Beltran <puentes.studio@gmail.com>
2025-03-12 11:23:02 +0000

    on touch for about image at home

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -6,23 +6,23 @@ import { Component } from '@angular/core';
   styleUrl: './about-me.component.css',
 })
 export class AboutMeComponent {
-  // Method to change the image when mouse is over the img
+  // Method to change the image on hover or touch start
   changeImage(newSrc: string) {
     const imgElement = document.querySelector(
       'img.about-img'
     ) as HTMLImageElement;
     if (imgElement) {
-      imgElement.src = newSrc; // Change to the hover image
+      imgElement.src = newSrc;
     }
   }
 
-  // Method to reset the image when mouse leaves
+  // Method to reset the image on mouseout or touchend
   resetImage() {
     const imgElement = document.querySelector(
       'img.about-img'
     ) as HTMLImageElement;
     if (imgElement) {
-      imgElement.src = 'assets/img/dev-img01.png'; // Reset to the original image
+      imgElement.src = 'assets/img/dev-img02.png'; // Reset to original image
     }
   }
 }
