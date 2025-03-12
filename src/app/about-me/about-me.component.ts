113031ff11826fb61f23e758c45b2cdb01459bc9
Vlad Beltran <puentes.studio@gmail.com>
2025-03-12 13:01:13 +0000

    hover effects for mobile on touch in about page done

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -25,4 +25,42 @@ export class AboutMeComponent {
       imgElement.src = 'assets/img/dev-img02.png'; // Reset to original image
     }
   }
+
+  applyTouchStyles() {
+    const titleElement = document.querySelector(
+      'h2.hello-title'
+    ) as HTMLElement;
+    if (titleElement) {
+      titleElement.classList.add('italic', 'text-yellow-300', 'no-underline');
+    }
+  }
+
+  // Method to remove styles on touchend or mouseout
+  removeTouchStyles() {
+    const titleElement = document.querySelector(
+      'h2.hello-title'
+    ) as HTMLElement;
+    if (titleElement) {
+      titleElement.classList.remove(
+        'italic',
+        'text-yellow-300',
+        'no-underline'
+      );
+    }
+  }
+
+  applyParagraphStyles() {
+    const paragraphElement = document.querySelector('p') as HTMLElement;
+    if (paragraphElement) {
+      paragraphElement.classList.add('bg-white', 'text-white');
+    }
+  }
+
+  // Method to remove styles on touchend or mouseout
+  removeParagraphStyles() {
+    const paragraphElement = document.querySelector('p') as HTMLElement;
+    if (paragraphElement) {
+      paragraphElement.classList.remove('bg-white', 'text-white');
+    }
+  }
 }
